6c6a0973d1499fe136a6a8c772ea3300d418e87f
Danny <danny.vaughton@gmail.com>
2023-04-25 14:25:33 +0100

    init commit

--- src/client-edit-me/edit-me/InstrumentReel.tsx
+++ src/client-edit-me/edit-me/InstrumentReel.tsx
@@ -2,8 +2,10 @@
  * ☑️ You can edit MOST of this file to add your own styles.
  */
 
-import { useCallback, useEffect, useRef, useState } from "react";
-import { Instrument, InstrumentSymbol } from "../../common-leave-me";
+/**
+ * ✅ You can add/edit these imports
+ */
+import { InstrumentSymbol } from "../../common-leave-me";
 import { InstrumentSocketClient } from "./InstrumentSocketClient";
 import "./InstrumentReel.css";
 
@@ -19,19 +21,6 @@ function useInstruments(instrumentSymbols: InstrumentSymbol[]) {
   /**
    * ✅ You can edit inside the body of this hook
    */
-  const [instruments, setInstruments] = useState<Instrument[]>([]);
-
-  useEffect(() => {
-    const unsubscribe = client.subscribe(instrumentSymbols, (instruments) => {
-      setInstruments(instruments);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [...instrumentSymbols]);
-
-  return instruments;
 }
 
 export interface InstrumentReelProps {
@@ -48,124 +37,8 @@ function InstrumentReel({ instrumentSymbols }: InstrumentReelProps) {
    * ✅ You can edit from here down in this component.
    * Please feel free to add more components to this file or other files if you want to.
    */
-  const animationRef = useRef<Animation>();
-  const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (containerRef.current && instrumentSymbols.length) {
-      animationRef.current?.cancel();
-      animationRef.current = containerRef.current.animate(
-        [
-          { transform: `translateX(0px)` },
-          {
-            transform: `translateX(-${containerRef.current.clientWidth / 2}px)`,
-          },
-        ],
-        {
-          duration: instrumentSymbols.length * 3_000,
-          iterations: Infinity,
-          easing: "linear",
-        }
-      );
-    }
-  }, [containerRef.current, ...instrumentSymbols]);
-
-  const onMouseEnter = useCallback(() => {
-    animationRef.current?.pause();
-  }, []);
-
-  const onMouseLeave = useCallback(() => {
-    animationRef.current?.play();
-  }, []);
-
-  if (!instrumentSymbols.length) {
-    return <div className="ir ir--empty">No Instruments</div>;
-  }
-
-  if (!instruments.length) {
-    return <div className="ir ir--loading">Loading Instruments...</div>;
-  }
-
-  return (
-    <div className="ir">
-      <div
-        className="ir__inner-wrapper ir__inner-wrapper--scroll"
-        ref={containerRef}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        {instruments.map((instrument) => (
-          <InstrumentReelItem key={instrument.code} instrument={instrument} />
-        ))}
-        {instruments.map((instrument) => (
-          <InstrumentReelItem
-            key={instrument.code + "-repeat-1"}
-            instrument={instrument}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export interface InstrumentReelItemProps {
-  instrument: Instrument;
-}
-
-function InstrumentReelItem({ instrument }: InstrumentReelItemProps) {
-  const prevQuoteRef = useRef(instrument.lastQuote);
-  const precision = instrument.category === "forex" ? 2 : 1;
-  const lastQuote = instrument.lastQuote.toFixed(precision);
-
-  useEffect(() => {
-    prevQuoteRef.current = instrument.lastQuote;
-  }, [instrument.lastQuote]);
-
-  const noDirClassName =
-    instrument.lastQuote > prevQuoteRef.current
-      ? "ir-item__number--up"
-      : instrument.lastQuote < prevQuoteRef.current
-      ? "ir-item__number--down"
-      : "";
-
-  const diff = instrument.lastQuote - prevQuoteRef.current;
-  const percChange = (diff / prevQuoteRef.current) * 100;
-  const dirSymbol = percChange > 0 ? "+" : "-";
-
-  return (
-    <div className="ir-item">
-      <InstrumentIcon instrument={instrument} />
-      <div className="ir-item__name">{instrument.name}</div>
-      <div className={`ir-item__number ${noDirClassName}`}>{lastQuote}</div>
-      <div className={`ir-item__number ${noDirClassName}`}>
-        <span>{dirSymbol}</span>
-        {Math.abs(percChange).toFixed(3)}
-        <span>%</span>
-      </div>
-    </div>
-  );
-}
-
-export interface InstrumentReelItemProps {
-  instrument: Instrument;
-}
-
-function InstrumentIcon({ instrument }: InstrumentReelItemProps) {
-  if (instrument.category === "forex") {
-    return (
-      <div className="ir-icon-pair">
-        <img src={`/${instrument.category}/${instrument.pair[0]}.svg`} />
-        <img src={`/${instrument.category}/${instrument.pair[1]}.svg`} />
-      </div>
-    );
-  }
-
-  return (
-    <img
-      className="ir-icon"
-      src={`/${instrument.category}/${instrument.code}.svg`}
-    />
-  );
+  return <div>Instrument Reel</div>;
 }
 
 export default InstrumentReel;
--- src/client-edit-me/edit-me/InstrumentSocketClient.ts
+++ src/client-edit-me/edit-me/InstrumentSocketClient.ts
@@ -2,6 +2,9 @@
  * ☑️ You can edit MOST of this file to add your own styles.
  */
 
+/**
+ * ✅ You can add/edit these imports
+ */
 import {
   Instrument,
   InstrumentSymbol,
@@ -10,6 +13,29 @@ import {
   WebSocketReadyState,
   WebSocketServerMessageJson,
 } from "../../common-leave-me";
+
+/**
+ * Notes:
+ * 
+ * To subscribe or unsubscribe to/from instrument(s), send a message to the server with the following format:
+ * 
+ * export type WebSocketClientMessageJson =
+  | {
+      type: "subscribe";
+      instrumentSymbols: InstrumentSymbol[];
+    }
+  | {
+      type: "unsubscribe";
+      instrumentSymbols: InstrumentSymbol[];
+    };
+  *
+  * The server will start responding with a message with the following format:
+  * 
+  * export type WebSocketServerMessageJson = {
+      type: "update";
+      instruments: Instrument[];
+    };
+ */
 
 /**
  * ❌ Please do not edit this class name
@@ -23,11 +49,6 @@ export class InstrumentSocketClient {
   /**
    * ✅ You can add more properties for the class here (if you want) 👇
    */
-  private _messageQueue: WebSocketMessage[] = [];
-  private _subscriptions = new Set<{
-    instrumentSymbols: InstrumentSymbol[];
-    callback: (instruments: Instrument[]) => void;
-  }>();
 
   constructor() {
     /**
@@ -38,100 +59,5 @@ export class InstrumentSocketClient {
     /**
      * ✅ You can edit from here down 👇
      */
-    this._socket.addEventListener("open", this._onOpen.bind(this));
-    this._socket.addEventListener("close", this._onClose.bind(this));
-    this._socket.addEventListener("error", this._onError.bind(this));
-    this._socket.addEventListener("message", this._onMessage.bind(this));
-  }
-
-  public subscribe(
-    instrumentSymbols: InstrumentSymbol[],
-    callback: (instruments: Instrument[]) => void
-  ) {
-    const subscription = {
-      instrumentSymbols,
-      callback,
-    };
-
-    this._subscriptions.add(subscription);
-    this._sendSubscriptionMessage("subscribe", instrumentSymbols);
-
-    return () => {
-      this._subscriptions.delete(subscription);
-
-      const remainingTracked = Array.from(this._subscriptions).flatMap(
-        (s) => s.instrumentSymbols
-      );
-
-      const unsubscribeInstruments = instrumentSymbols.filter(
-        (t) => !remainingTracked.includes(t)
-      );
-
-      if (unsubscribeInstruments.length) {
-        this._sendSubscriptionMessage("unsubscribe", unsubscribeInstruments);
-      }
-    };
-  }
-
-  public dispose() {
-    this._socket.close();
-    this._socket.removeEventListener("open", this._onOpen.bind(this));
-    this._socket.removeEventListener("close", this._onClose.bind(this));
-    this._socket.removeEventListener("error", this._onError.bind(this));
-    this._socket.removeEventListener("message", this._onMessage.bind(this));
-  }
-
-  private _onInstrumentUpdate(instruments: Instrument[]) {
-    this._subscriptions.forEach((s) => {
-      s.callback(
-        instruments.filter((t) => s.instrumentSymbols.includes(t.code))
-      );
-    });
-  }
-
-  private _sendSubscriptionMessage(
-    type: "subscribe" | "unsubscribe",
-    instrumentSymbols: InstrumentSymbol[]
-  ) {
-    const json: WebSocketClientMessageJson = {
-      type,
-      instrumentSymbols,
-    };
-    this._sendMessage(JSON.stringify(json));
-  }
-
-  private _sendMessage(message: WebSocketMessage) {
-    if (this._socket.readyState === WebSocketReadyState.OPEN) {
-      console.log("sending", message);
-      this._socket.send(message);
-    } else {
-      this._messageQueue.push(message);
-    }
-  }
-
-  private _onOpen() {
-    this._messageQueue.splice(0).forEach((message) => {
-      this._sendMessage(message);
-    });
-  }
-
-  private _onMessage(message: MessageEvent<any>) {
-    try {
-      const data = JSON.parse(message.data) as WebSocketServerMessageJson;
-      switch (data.type) {
-        case "update":
-          this._onInstrumentUpdate(data.instruments);
-      }
-    } catch (e) {
-      console.error("Could not parse message", e);
-    }
-  }
-
-  private _onClose() {
-    console.log("onClose");
-  }
-
-  private _onError(e: Event) {
-    console.error("onError", e);
   }
 }
