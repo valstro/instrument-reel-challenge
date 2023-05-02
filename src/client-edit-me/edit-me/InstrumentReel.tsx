dfdb32de50870d8011eb5e613aa06307bc51c679
a-amato3 <aaronamato@hotmail.com>
2023-05-02 18:33:13 +0100

    added correct animation effects as well as styling to show correct instrument symbols and other metadata

--- src/client-edit-me/edit-me/InstrumentReel.tsx
+++ src/client-edit-me/edit-me/InstrumentReel.tsx
@@ -27,29 +27,69 @@ function useInstruments(instrumentSymbols: InstrumentSymbol[]) {
    * ✅ You can edit inside the body of this hook
    */
   const [instruments, setInstruments] = useState<Instrument[]>([]);
+  const [previousQuotes, setPreviousQuotes] = useState<{
+    [key: string]: number;
+  }>({});
+
   useEffect(() => {
+    // Subscribe to websocket for instrument update
     const handleUpdate = (message: WebSocketServerMessageJson) => {
       if (message.type === "update") {
-        setInstruments(message.instruments);
+        const updatedInstruments = message.instruments.map((instrument) => {
+          const previousQuote = previousQuotes[instrument.code];
+          return {
+            ...instrument,
+            previousQuote,
+          };
+        });
+
+        // Update instrument state and previous quote state
+        setInstruments(updatedInstruments);
+        message.instruments.forEach((instrument) => {
+          setPreviousQuotes((prev) => ({
+            ...prev,
+            [instrument.code]: instrument.lastQuote,
+          }));
+        });
       }
     };
 
-    /**
-     * ❌ subscribe not working
-     */
-
     client.subscribe(instrumentSymbols, handleUpdate);
 
+    // Unsubscribe from websocket when component is unmounted
     return () => {
       client.unsubscribe(instrumentSymbols, handleUpdate);
     };
-  }, [instrumentSymbols]);
+  }, [instrumentSymbols, previousQuotes]);
 
   return instruments;
 }
 
 export interface InstrumentReelProps {
   instrumentSymbols: InstrumentSymbol[];
+}
+
+function getInstrumentIcon(code: InstrumentSymbol): JSX.Element {
+  // Return a custom icon for EUR/USD currency pair
+  if (code === "EURUSD") {
+    return (
+      <>
+        <div className="instrument-icon">
+          <img className="USD" src={"EUR.svg"} alt="EUR" />
+          <img className="EUR" src={"USD.svg"} alt="USD" />
+        </div>
+      </>
+    );
+  } else {
+    // Return an icon with the currency code as its image source for all other instruments
+    return <img className="instrument-icon" src={`${code}.svg`} alt={code} />;
+  }
+}
+
+function getPercentageChange(current: number, previous: number): string {
+  const change = ((current - previous) / previous) * 100;
+  const sign = previous < current ? "+" : "";
+  return sign + change.toString();
 }
 
 function InstrumentReel({ instrumentSymbols }: InstrumentReelProps) {
@@ -64,17 +104,62 @@ function InstrumentReel({ instrumentSymbols }: InstrumentReelProps) {
    */
 
   return (
-    <div className="instrument-reel">
+    <div className="instrument-reel ">
       {instruments.length > 0 ? (
         <div className="instrument-list">
           {instruments.map((instrument: Instrument) => (
             <div key={instrument.code} className="instrument-item">
-              <img
-                className="instrument-icon"
-                src={`../../../public/${instrument.code}.svg`}
-                alt={instrument.code}
-              />
-              {instrument.code} {instrument.lastQuote}
+              <div className="instrument-info">
+                <div className="instrument-icon">
+                  {getInstrumentIcon(instrument.code)}
+                </div>
+                <div className="instrument-name">{instrument.name}</div>
+                <div
+                  className={`instrument-quotes ${
+                    //@ts-ignore
+                    instrument.previousQuote < instrument.lastQuote
+                      ? "positive"
+                      : //@ts-ignore
+                      instrument.previousQuote > instrument.lastQuote
+                      ? "negative"
+                      : ""
+                  }`}
+                >
+                  {instrument.lastQuote.toFixed(1)}
+                </div>
+
+                <div
+                  className={`instrument-quotes ${
+                    Number(
+                      getPercentageChange(
+                        instrument.lastQuote,
+                        //@ts-ignore
+                        instrument.previousQuote
+                      )
+                    ) < 0
+                      ? "negative"
+                      : "positive"
+                  }`}
+                >
+                  {getPercentageChange(
+                    instrument.lastQuote,
+                    //@ts-ignore
+                    instrument.previousQuote
+                  )[0] === "-"
+                    ? "-"
+                    : "+"}
+                  {Math.abs(
+                    Number(
+                      getPercentageChange(
+                        instrument.lastQuote,
+                        //@ts-ignore
+                        instrument.previousQuote
+                      )
+                    )
+                  ).toFixed(3)}
+                  %
+                </div>
+              </div>
             </div>
           ))}
         </div>
--- src/client-edit-me/edit-me/InstrumentSocketClient.ts
+++ src/client-edit-me/edit-me/InstrumentSocketClient.ts
@@ -46,7 +46,10 @@ export class InstrumentSocketClient {
    */
   private _socket: WebSocket;
   private _callbacks: {
-    [key: string]: (message: WebSocketServerMessageJson) => void;
+    [key: string]: {
+      callback: (message: WebSocketServerMessageJson) => void;
+      prevQuote: Instrument | null;
+    };
   } = {};
 
   instrumentSymbols: InstrumentSymbol[] = [];
@@ -56,53 +59,87 @@ export class InstrumentSocketClient {
     instrumentSymbols: this.instrumentSymbols,
   };
   constructor() {
+    /**
+     * This creates a new WebSocket object that connects to the server at "ws://localhost:3000/ws".
+     *
+     * The WebSocket object is saved as a private property of the client object for use in other methods.
+     */
     this._socket = new WebSocket("ws://localhost:3000/ws");
 
+    /**
+     * This sends the initial subscribe message to the server when the WebSocket connection is opened.
+     *
+     * The initial subscribe message contains an empty array of instrument symbols.
+     * The actual instrument symbols that the client is subscribed to are added later with the subscribe method.
+     */
     this._socket.onopen = () => {
       this._socket.send(JSON.stringify(this.subscribeMessage));
-
-      console.log("subscribeMessage MESSAGE", this.subscribeMessage);
     };
 
+    /**
+     * This processes incoming messages from the server.
+     * If the message is an "update" message, it calls all of the registered callback functions with the updated instrument data.
+     * If the message is not an "update" message, it is ignored.
+     */
     this._socket.onmessage = (event: MessageEvent) => {
       const message = JSON.parse(event.data);
-      // if (message.type === "update") {
-      const callbacks = Object.values(this._callbacks);
-      console.log("INSTRUMENT callbacks", callbacks);
-      console.log("INSTRUMENT MESSAGE", message);
-
-      callbacks.forEach((callback) => callback(message));
-      // }
+      if (message.type === "update") {
+        const callbacks = Object.values(this._callbacks);
+        callbacks.forEach(({ callback, prevQuote }) => {
+          message.instruments.forEach((newQuote: any) => {
+            if (newQuote.code === prevQuote?.code) {
+              newQuote = { ...prevQuote, ...newQuote };
+            }
+          });
+          callback(message);
+        });
+      }
     };
   }
 
+  /**
+   * The subscribe method takes an array of instrumentSymbols to subscribe to and a callback function that is called every time an update is received from the server.
+   * It generates a unique identifier for the callback and stores it along with the prevQuote property in the _callbacks object.
+   * It also sends a subscribeMessage to the server to start receiving updates.
+   *
+   * @param {InstrumentSymbol[]} instrumentSymbols - An array of instrument symbols to subscribe to.
+   * @param {Function} callback - A function to be called every time an update is received from the server.
+   * @returns {string} - A unique identifier for the callback function.
+   */
   public subscribe(
     instrumentSymbols: InstrumentSymbol[],
     callback: (message: WebSocketServerMessageJson) => void
-  ) {
-    const id: string = Math.random().toString(36).substr(2, 9);
-    this._callbacks[id] = callback;
+  ): string {
+    const id: string = Math.random().toString();
+    this._callbacks[id] = {
+      callback,
+      prevQuote: null,
+    };
 
     const subscribeMessage: WebSocketClientMessageJson = {
       type: "subscribe",
       instrumentSymbols: instrumentSymbols,
     };
-    console.log("INSTRUMENT MESSAGE", subscribeMessage);
 
     if (this._socket.readyState === WebSocketReadyState.OPEN) {
       this._socket.send(JSON.stringify(subscribeMessage));
     }
+
     return id;
   }
 
+  /**
+   * The unsubscribe method takes an array of instrumentSymbols and a callback function that was previously registered with the subscribe method.
+   * It finds the unique identifier associated with the callback function in the _callbacks object, deletes it,
+   * and sends an unsubscribeMessage to the server to stop receiving updates.
+   */
   public unsubscribe(
     instrumentSymbols: InstrumentSymbol[],
     callback: (message: WebSocketServerMessageJson) => void
   ): void {
     const id = Object.keys(this._callbacks).find(
-      (key) => this._callbacks[key] === callback
+      (key) => this._callbacks[key].callback === callback
     );
-
     if (id) {
       delete this._callbacks[id];
 
@@ -111,7 +148,9 @@ export class InstrumentSocketClient {
         instrumentSymbols: instrumentSymbols,
       };
 
-      this._socket.send(JSON.stringify(unsubscribeMessage));
+      if (this._socket.readyState === WebSocketReadyState.OPEN) {
+        this._socket.send(JSON.stringify(unsubscribeMessage));
+      }
     }
   }
 }
