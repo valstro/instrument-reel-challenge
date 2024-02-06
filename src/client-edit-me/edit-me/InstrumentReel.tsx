5a817a45db3c269179ea1ef4a7d4e1cb5d81a349
alexcox <alexncox@gmail.com>
2024-02-06 11:24:03 +0100

    Render instrument data from websocket updates
    TODO: style, animate, and pause instrument reel
    TODO: separate concerns in component and InstrumentSocketClient
    TODO: test

--- src/client-edit-me/edit-me/InstrumentReel.tsx
+++ src/client-edit-me/edit-me/InstrumentReel.tsx
@@ -5,8 +5,10 @@
 /**
  * ✅ You can add/edit these imports
  */
-import { InstrumentSymbol } from "../../common-leave-me";
+import React from "react";
+import { InstrumentSymbol, WebSocketReadyState } from "../../common-leave-me";
 import { InstrumentSocketClient } from "./InstrumentSocketClient";
+
 import "./InstrumentReel.css";
 
 /**
@@ -17,10 +19,49 @@ const client = new InstrumentSocketClient();
 /**
  * ❌ Please do not edit this hook name & args
  */
+
+type InstrumentReelItem = [InstrumentSymbol, number | string];
+
 function useInstruments(instrumentSymbols: InstrumentSymbol[]) {
   /**
    * ✅ You can edit inside the body of this hook
    */
+  const pendingInstruments = instrumentSymbols.map(
+    (symbol) => [symbol, "..."] as InstrumentReelItem
+  );
+  const [instruments, setInstruments] =
+    React.useState<InstrumentReelItem[]>(pendingInstruments);
+  const [readyState, setReadyState] = React.useState<WebSocketReadyState>(
+    client.readyState
+  );
+
+  const handleMessage = (newInstruments: any[]) => {
+    setInstruments(newInstruments);
+  };
+
+  React.useEffect(() => {
+    const handleReadyStateChange = () => {
+      setReadyState(client.readyState);
+    };
+    handleReadyStateChange();
+    client.addEventListener("open", handleReadyStateChange);
+    client.addEventListener("close", handleReadyStateChange);
+    return () => {
+      client.removeEventListener("open", handleReadyStateChange);
+      client.removeEventListener("close", handleReadyStateChange);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (readyState !== WebSocket.OPEN) {
+      return;
+    }
+    client.subscribeToSymbolUpdates(instrumentSymbols, handleMessage);
+    return () => {
+      client.unsubscribeFromSymbolUpdates(instrumentSymbols, handleMessage);
+    };
+  }, [instrumentSymbols, readyState]);
+  return instruments;
 }
 
 export interface InstrumentReelProps {
@@ -38,7 +79,17 @@ function InstrumentReel({ instrumentSymbols }: InstrumentReelProps) {
    * Please feel free to add more components to this file or other files if you want to.
    */
 
-  return <div>Instrument Reel</div>;
+  return (
+    <div className="foo">
+      {instruments.map(([symbol, lastQuote], i) => {
+        return (
+          <div key={i}>
+            {symbol}: {lastQuote}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default InstrumentReel;
--- src/client-edit-me/edit-me/InstrumentSocketClient.ts
+++ src/client-edit-me/edit-me/InstrumentSocketClient.ts
@@ -36,7 +36,7 @@ import {
       instruments: Instrument[];
     };
  */
-
+const DEBUGGING = true;
 /**
  * ❌ Please do not edit this class name
  */
@@ -50,14 +50,130 @@ export class InstrumentSocketClient {
    * ✅ You can add more properties for the class here (if you want) 👇
    */
 
+  private _subscriptions: Map<any, any>;
+  static sockets: WebSocket[] = [];
+
   constructor() {
     /**
      * ❌ Please do not edit this private property assignment
      */
+
+    // note: even though this is instantiated once in module scope in the component tsx file,
+    // hot reloads rerun that file, causing multiple sockets to be created and not removed
     this._socket = new WebSocket("ws://localhost:3000/ws");
 
     /**
      * ✅ You can edit from here down 👇
      */
+    this.globalListeners.forEach(([eventType, callback]) => {
+      this._socket.addEventListener(eventType, callback);
+    });
+    InstrumentSocketClient.sockets.push(this._socket);
+
+    this._subscriptions = new Map();
+  }
+
+  // DEBUGGING
+  private globalListeners: any[] = [
+    // ["open", () => console.log("websocket open")],
+    DEBUGGING && [
+      "message",
+      (event: MessageEvent) =>
+        console.log(
+          "Message from server ",
+          JSON.parse(event.data).instruments.map((i: any) => i.code)
+        ),
+    ],
+  ].filter((l) => l) as any[];
+
+  close() {
+    if (DEBUGGING) {
+      this.globalListeners.forEach(([eventType, callback]) => {
+        this._socket.removeEventListener(eventType, callback);
+      });
+      InstrumentSocketClient.sockets = InstrumentSocketClient.sockets.filter(
+        (socket) => socket !== this._socket
+      );
+    }
+    this._socket.close();
+  }
+
+  get readyState(): WebSocketReadyState {
+    return this._socket.readyState;
+  }
+
+  private _sendMessage(message: WebSocketClientMessageJson) {
+    this._socket.send(JSON.stringify(message));
+  }
+
+  private _parseInstrumentsMessage(data: any) {
+    const message = JSON.parse(data);
+    return Object.fromEntries(
+      message.instruments.map((i: any) => [i.code, i.lastQuote])
+    );
+  }
+
+  addEventListener(eventType: string, callback: (event: Event) => void) {
+    this._socket.addEventListener(eventType, callback);
+  }
+
+  removeEventListener(eventType: string, callback: (event: Event) => void) {
+    this._socket.removeEventListener(eventType, callback);
+  }
+
+  // TODO: DISCUSS:
+  // subscribeToSymbolUpdates couples the server-side subscription to the message listener.
+  // They seem like the same concern to me, but it feels cleaner maybe to separate them?
+  // e.g.
+  //
+  // requestInstrumentUpdates(instrumentSymbols: InstrumentSymbol[]) {
+  //   this._sendMessage({
+  //     type: "subscribe",
+  //     instrumentSymbols,
+  //   });
+  // });
+  //
+  // onInstrumentUpdate(callback: (newInstruments: any[]) => void) {
+  //   const handler = (event: any) => {
+  //     const updates = this._parseInstrumentsMessage(event.data);
+  //     const newInstruments: any = instrumentSymbols.map((symbol) => [
+  //       symbol,
+  //       updates[symbol],
+  //     ]);
+  //     callback(newInstruments);
+  //   };
+  //   this._socket.addEventListener("message", handler);
+  //   return () => this._socket.removeEventListener("message", handler);
+  // }
+  subscribeToSymbolUpdates(
+    instrumentSymbols: InstrumentSymbol[],
+    callback: (newInstruments: any[]) => void
+  ) {
+    this._sendMessage({
+      type: "subscribe",
+      instrumentSymbols,
+    });
+    const handler = (event: any) => {
+      const updates = this._parseInstrumentsMessage(event.data);
+      const newInstruments: any = instrumentSymbols.map((symbol) => [
+        symbol,
+        updates[symbol],
+      ]);
+      callback(newInstruments);
+    };
+    this._subscriptions.set(callback, handler);
+    this._socket.addEventListener("message", handler);
+  }
+
+  unsubscribeFromSymbolUpdates(
+    instrumentSymbols: InstrumentSymbol[],
+    callback: (newInstruments: any[]) => void
+  ) {
+    this._sendMessage({
+      type: "unsubscribe",
+      instrumentSymbols,
+    });
+    const handler = this._subscriptions.get(callback);
+    this._socket.removeEventListener("message", handler);
   }
 }
